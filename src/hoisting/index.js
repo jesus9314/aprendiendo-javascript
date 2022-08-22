595e86d25ed7117cbafe35146760e6f786ed9bf5
Jesus Ale <jesus.9314@gmail.com>
2022-08-22 00:18:15 -0500

    hoisting

---
+++ src/hoisting/index.js
@@ -0,0 +1,12 @@
+// var nameOfDog //declara esa variable y ya que no hay un valor, le coloca undefined
+
+// console.log(nameOfDog) //undefined
+// var nameOfDog = 'Elmo' 
+
+nameOfDog() //El mejor perrito es undefined
+
+function nameOfDog(){ //la función tambien es elevada
+    console.log(`El mejor perrito es ${elmo}`)
+}
+
+var elmo = 'Elmito'
